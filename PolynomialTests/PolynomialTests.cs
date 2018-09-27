75124972ed26915a7754024a4f7ce51cd9fa3d47
odesyatnyk <odesyatnyk@mail.ru>
2018-09-28 00:48:11 +0300

    cleanup

--- PolynomialTests/PolynomialTests.cs
+++ PolynomialTests/PolynomialTests.cs
@@ -11,34 +11,6 @@ namespace PolynomialNamespace.Tests
     [TestFixture]
     public class PolynomialTests
     {
-        [Test]
-        public void PolynomialTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void PolynomialTest1()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void PolynomialTest2()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void ToStringTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        public void CloneTest()
-        {
-            Assert.Fail();
-        }
+        
     }
 }
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -12,26 +12,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Dictionary<int, int> dic = new Dictionary<int, int>
-            {
-                { 5, 0 },
-                { 4, 0 },
-                { 3, 9 }
-            };
-            Polynomial dicpol = new Polynomial(dic);
 
-            Polynomial pol = new Polynomial(new List<int>() { 0, 0, 9, 0, 0, 0 });
-            Polynomial pol2 = new Polynomial(new List<int>() { 10, 0, 9, 0, 0, 0 });
-            //pol2[50] = 1;
-
-            Polynomial pol3 = pol2 - pol2 + pol;
-
-            //Matrix mat = new Matrix(4, 4, 0, 4);
-            //mat.SaveMatrixToBinaryFile("file");
-            //Matrix mat2 = new Matrix();
-            //mat2 = Matrix.ReadMatrixFromBinaryFile("file");
-
-            Console.ReadKey();
         }
     }
 }
